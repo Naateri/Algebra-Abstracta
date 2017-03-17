5da662b32ba2b72786ce645b80b7a0da00838739
Naateri <renato.postigo@ucsp.edu.pe>
2017-03-17 18:16:09 -0500

    Cifrado de Cesar
    Cifrado de Cesar

---
+++ cesar.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//Cifrado de Cesar
+class Cesar{
+private:
+	string alfabeto;
+	int key;
+public:
+	Cesar(){
+		alfabeto = "abcdefghijklmnopqrstuvwxyz";
+		key = 3;
+	}
+	string cifrar(string msj){
+		int i = 0, found;
+		string cifrado, letra;// = "";
+		for (i; i<msj.length(); i++){
+			letra = msj[i];
+			if (letra == " "){
+				cifrado += " ";
+				continue;
+			}
+			found = alfabeto.find(letra);
+			//cout << found << endl;
+			if (found > alfabeto.length()){
+					cout << "Error. Letra no encontrada.\n";
+					msj = "Error.\n";
+					return msj;
+			}
+			if (found+key > alfabeto.length()){
+				found -= 26; 
+			}
+			cifrado += alfabeto[found+key];
+			
+		}
+		return cifrado;
+	}
+	string descifrar(string cifrado){ 
+		int i = 0, found;
+		string descifrado, letra;
+		for (i; i<cifrado.length(); i++){
+			letra = cifrado[i];
+			if (letra == " "){
+				descifrado += " ";
+				continue;
+			}
+			found = alfabeto.find(letra);
+			if (found > alfabeto.length()){
+				cout << "Error. Letra no encontrada.\n";
+				cifrado = "Error.\n";
+				return cifrado;
+			} 
+			if (found-key < 0){
+				found += 26;
+			}
+			descifrado += alfabeto[found-key];
+		}
+		return descifrado;
+	}
+	int generarClaves(){
+		;
+	}
+	
+	
+};
+
+void acciones(){
+	string mensaje, cifrado, descifrado;
+	while(1){
+		cout << "Ingrese su mensaje (0 para salir): ";
+		getline(cin, mensaje);
+		if (mensaje == "0")
+			break;
+		cout << "Mensaje original: " << mensaje << endl;
+		Cesar Algoritmo;
+		cifrado = Algoritmo.cifrar(mensaje);
+		cout << "Mensaje cifrado: " << cifrado << endl;
+		descifrado = Algoritmo.descifrar(cifrado);
+		cout << "Mensaje descifrado: " << descifrado << endl;
+		mensaje = "";
+	}
+}
+	
+int main() {
+	acciones();
+	return 0;
+}
+
