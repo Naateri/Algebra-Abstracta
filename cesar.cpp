364278f42e70149b0d23e6ed0d304518e16dae42
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-03-23 20:00:50 -0500

    Cesar NO completado

--- cesar.cpp
+++ cesar.cpp
@@ -9,13 +9,13 @@ class Cesar{
 private:
 	string alfabeto;
 	int key;
-	int generarClaves(){
+	/*int generarClaves(){
 		return rand() % 10 + 1;
-	}
+	}*/
 public:
-	Cesar(){
+	Cesar(int k){
 		this -> alfabeto = "abcdefghijklmnopqrstuvwxyz ";
-		this -> key = generarClaves();
+		this -> key = k;
 	}
 	string cifrar(string msj){
 		int i = 0, found;
@@ -23,9 +23,13 @@ public:
 		for (i; i<msj.length(); i++){
 			letra = msj[i];
 			found = alfabeto.find(letra);
-			//cout << found << endl;
-			if (found+key > alfabeto.length())
-				found = modulo(found, alfabeto.length());
+			if (found > alfabeto.find('z')){
+				cifrado += alfabeto[found]; //arreglar estoooo
+				continue;
+			}
+			found += this->key;
+			if (found > alfabeto.length() - 1)
+				found = modulo(found, alfabeto.length() - 1);
 			cifrado += alfabeto[found];
 			
 		}
@@ -37,8 +41,13 @@ public:
 		for (i; i<cifrado.length(); i++){
 			letra = cifrado[i];
 			found = alfabeto.find(letra);
-			if (found-key < 0){
-				found = modulo(found, alfabeto.length());
+			if (found > alfabeto.find('z')){
+				descifrado += alfabeto[found]; //arreglar estooooooooo
+				continue;
+			}
+			found -= this->key;
+			if (found< 0){
+				found = modulo(found, alfabeto.length()-1);
 			}
 			descifrado += alfabeto[found];
 		}
@@ -49,23 +58,23 @@ public:
 	
 };
 void acciones(){
-	string mensaje = "1", cifrado, descifrado;
+	string mensaje, cifrado, descifrado;
 	int key;
+	cin >> key;
 	while(mensaje != "0"){
 		cout << "Ingrese su mensaje (0 para salir): ";
 		getline(cin, mensaje);
-		/*if (mensaje == "0")
-			break;*/
+		/*cout << "Ingrese clave: ";
+		cin >> key;*/
 		cout << "Mensaje original: " << mensaje << endl;
-		Cesar Algoritmo;
-		cout << "Ingrese clave: ";
-		cin >> key;
+		Cesar Algoritmo(key);
+		//Algoritmo.setKey(key);
 		cifrado = Algoritmo.cifrar(mensaje);
 		cout << "Clave: " << Algoritmo.getKey() << endl;
 		cout << "Mensaje cifrado: " << cifrado << endl;
 		descifrado = Algoritmo.descifrar(cifrado);
 		cout << "Mensaje descifrado: " << descifrado << endl;
-		mensaje = "";
+		//key = 0;
 	}
 	cout << "Hasta luego!\n";
 }
