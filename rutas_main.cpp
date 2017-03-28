8fa2dfecb74cc7b26f77a4c005c8faabb4863b95
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-03-28 13:28:37 -0500

    testing rutas

---
+++ rutas_main.cpp
@@ -0,0 +1,34 @@
+//23-03-2017
+/*
+mensaje: holacomoesta
+clave: 4
+h     m     X
+ o   o o   a
+  l c   e t
+   a     s
+Mensaje cifrado: 
+hmXoooalcetas
+Mensaje cifrado:
+holacomoesta
+*/
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "maths.h"
+#include "rutas.h"
+using namespace std;
+int main(){
+	string msj;
+	int key;
+	cout << "Ingrese su mensaje: ";
+	getline(cin, msj);
+	cout << "Ingrese la clave: ";
+	cin >> key;
+	Rutas Algoritmo(key);
+	//Algoritmo.setKey(key);
+	msj = Algoritmo.cifrar(msj);
+	cout << "Mensaje cifrado: " << msj << endl;
+	msj = Algoritmo.descifrar(msj);
+	cout << "Mensaje descifrado: " << msj << endl;
+	return 0;
+}
