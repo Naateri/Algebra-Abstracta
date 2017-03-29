a661b47223f3d5e4df417c4b1545123cb49df991
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-03-29 13:58:44 -0500

    Cifrado ya hecho... casi. REVISAR

--- rutas.cpp
+++ rutas.cpp
@@ -26,25 +26,34 @@ string Rutas::cifrar(string msj){
 	bool sube; //para ver si sube
 	int i = 0, j = 0, k;
 	while(i < msj.length()){
-		k = this->key;
-		cout << "Key: " << k << endl;
+		k = this->key -1;		
+		cout << "Key - 1: " << k << endl;
+		if (i == 0){	//primer paso
+			messages[j] += msj[i];
+			i++;
+			j++;
+			continue;		
+		}
 		if (j >= k) 
 			sube = true;
 		else sube = false;
-		//messages[j] += msj[i];
 		for(k; k > 0; k--){ //not always the same number of loops
 			//messages[j] += msj[i];
-			cout << j << endl;			
-			if (sube){ //booleano que debe mantenerse igual por k iteraciones
-				messages[j] += msj[i];				
+			cout << "k: " << k << endl;			
+			if (sube){ //booleano que debe mantenerse igual por k iteraciones				
 				j--;
+				messages[j] += msj[i];
+				cout << "j: " << j << endl;
 			}
 			else{
 				messages[j] += msj[i];
 				j++;
+				cout << "j: " << j << endl;
 			}
 			i++;
 		}
+		if (!sube)
+			j--;
 	}
 	for (i = 0; i<this->key; i++)
 		cifrado += messages[i];
