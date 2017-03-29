6ea613ad088bb1c0af49af70023e9c2f7c46dbcc
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-03-29 12:54:16 -0500

    cesar main

--- cesar_main.cpp
+++ cesar_main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
-#include "maths.h"
 #include "cesar.h"
-using namespace std;
-//Cifrado de Cesar
-//Clave ingresada por el usuario
-	
-int main() {
-	string mensaje, cifrado, descifrado;
-	int key;
-	//while(mensaje != "0"){
-		cout << "Ingrese su mensaje: ";
-		getline(cin, mensaje);
-		cout << "Ingrese clave: ";
-		cin >> key;
-		cout << "Mensaje original: " << mensaje << endl;
-		Cesar Algoritmo(key);
-		//Algoritmo.setKey(key);
-		cifrado = Algoritmo.cifrar(mensaje);
-		cout << "Clave: " << Algoritmo.getKey() << endl;
-		cout << "Mensaje cifrado: " << cifrado << endl;
-		descifrado = Algoritmo.descifrar(cifrado);
-		cout << "Mensaje descifrado: " << descifrado << endl;
-	//}
-	return 0;
-}
+#include "maths.h"
 
+using namespace std;
+
+int main(){
+	int key;
+	string message, cifrado, descifrado;
+	cout << "Ingrese su mensaje: ";
+	getline(cin, message);
+	cout << "Ingrese la clave: ";
+	cin >> key;
+	Cesar Algoritmo(key);
+	std::cout << "Mensaje cifrado:\n";
+	cifrado = Algoritmo.cifrar(message);
+	std::cout << cifrado << endl;
+	descifrado = Algoritmo.descifrar(cifrado);
+	std::cout << "Mensaje descifrado\n" << descifrado << endl;
+	
+}
