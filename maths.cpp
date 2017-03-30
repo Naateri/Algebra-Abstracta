a0c722436acc3cd50fc56e178c7a0233ee530890
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-03-29 23:49:28 -0500

    euclides extendido usando par

--- maths.cpp
+++ maths.cpp
@@ -1,16 +1,26 @@
 //maths.cpp
 //algebra abstracta
 #include "maths.h"
-#include <vector>
+#include <utility>
+using namespace std;
+
 int modulo(int a, int n){
 	int q, r;
 	q = a/n;
 	r = a - (q*n);
-	if (r<0){
-		q--;
+	if (r<0)
 		r += n;
-	}
 	return r;
+}
+
+pair<int, int> moduloQandR(int a, int n){
+	int q, r;
+	q = a/n;
+	r = a - (q*n);
+	if (r<0)
+		r+=n;
+	pair<int, int> values(r, q);
+	return values;
 }
 
 int mcd(int a, int b){ //mcd iterativo 
@@ -23,21 +33,23 @@ int mcd(int a, int b){ //mcd iterativo
 	return b;
 }
 
-std::vector<int> mcdExtendido(int a, int b){
+pair<int, int> mcdExtendido(int a, int b){
 	int r, r1 = a, r2 = b, x, x1 = 1, x2 = 0, y, y1 = 0, y2 = 1, q;
-	while (r2 > 0){
-		q = r1/r2; //EUCLIDES
-		r = r1 - q*r2; //EUCLIDES
-		r1 = r2; r2 = r; //EUCLIDES (como borrar?)
+	pair<int, int> modulo;
+	while (r2 != 0){
+		modulo = moduloQandR(r1, r2);
+		q = modulo.second; //EUCLIDES
+		r = modulo.first; //EUCLIDES
+		r1 = r2;
+		r2 = r; 
 		x = x1 - q*x2;
-		x1 = x2; x2 = x;
+		x1 = x2; 
+		x2 = x;
 		y = y1 - q * y2;
-		y1 = y2; y2 = y;
+		y1 = y2; 
+		y2 = y;
 	}
-	std::vector<int> resultados;
-	resultados.push_back(x); //resultados.at(0) = x
-	resultados.push_back(y); //resultados.at(1) = y
-	resultados.push_back(r1); //resultados.at(2) = mcd(a,b)
-	return resultados;
+	pair<int, int> resultados(x,y); //resultados.first = x
+	return resultados; //resultados.second = y
 }
 
--- maths.h
+++ maths.h
@@ -1,11 +1,12 @@
 //maths.h 
 //operaciones de algebra abstracta
+#include <utility>
 #ifndef MATHS_H_INCLUDED
 #define MATHS_H_INCLUDED
-#include <vector>
 int modulo(int a, int n);
+std::pair<int, int> moduloQandR(int a, int n);
 int mcd(int a, int b);
-std::vector<int> mcdExtendido(int a, int b);
+std::pair<int, int> mcdExtendido(int a, int b);
 #endif 
 
 
