fe45ec987df00c866dfff058bef3c2145faced5e
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-04-05 23:55:40 -0500

    descifra mas casos... casi. seguir trabajando

--- rutas.cpp
+++ rutas.cpp
@@ -69,12 +69,12 @@ string Rutas::descifrar(string cifrado){ //funciona hasta que da más de una 'vu
 	string messages[this->key]; //para guardar los mensajes
 	string descifrado, part; //guarda el descifrado
 	char partComparison;
-	int i = 0, j = 0, sizeofpart, k = 0;
+	int sizeofpart, j = 0;
+	short i = 0, k = 0, zero = 0; //zero = 0 si solo ha dado una vuelta al descifrar
 	sizeofpart = cifrado.size()/this->key; //2 casos: si es 1 o si no es 1
-	j = sizeofpart+1;
 	while(i<cifrado.size()){ //ordenando en los arrays
 		part.clear();		
-		if (sizeofpart == 1){ //loop cumple su funcion
+		if (sizeofpart == 1){
 			j = sizeofpart+1;
 			while(j>0 && i<cifrado.size()){
 				part += cifrado[i];
@@ -84,7 +84,7 @@ string Rutas::descifrar(string cifrado){ //funciona hasta que da más de una 'vu
 			messages[k] = part;
 			k++;
 		} else { //guarda el mensaje por partes en el array 
-			if (i < sizeofpart)
+			if (i < sizeofpart) //messages[0] guarda sizeofpart-1 caracteres
 				j = sizeofpart;
 			else j = sizeofpart+1;	
 			while(j>0 && i<cifrado.size()){
@@ -95,7 +95,7 @@ string Rutas::descifrar(string cifrado){ //funciona hasta que da más de una 'vu
 			messages[k] = part;
 			k++;
 		}
-	} //end while
+	}
 	i = 0; j = 0; k = 0;
 	while(i<cifrado.size()){
 		bool regresa;
@@ -103,27 +103,35 @@ string Rutas::descifrar(string cifrado){ //funciona hasta que da más de una 'vu
 			regresa = true;
 			j--;
 			cout << "j: " << j << endl;
+		} else {
+			regresa = false;
+			if (zero != 0){ //no es la primera vez que j = 0
+				j++;
+				}
+			else zero++;
+				//k++;
 		}
-		else regresa = false;
-		while(j < this->key && j >= 0 && i < cifrado.size()){ //j>0 no sale del loop j>=0 bad_alloc
+		while(j < this->key && j >= 0 && i < cifrado.size()){ //j>0 no sale del loop
 			cout << "entering loop, j: " << j << endl;
-			cout << "valor de k: " << k << endl;
-			if (j == 0)
-				regresa = false;
-			//j = celda, k = posicion en la palabra
+			cout << "valor de k: " << k << endl; //j = celda, k = posicion en la palabra
 			if (regresa){
+				cout << "Regresa (-)\n";
 				j--;
+				if (j<0)
+					//j *= -1;
+					break;
 				part = messages[j];
 				cout << "guardado en la parte " << j << ": " << part << endl;
 				descifrado += part[k];
 			} else {
+				cout << "!regresa (+)\n";
 				part = messages[j];
 				cout << "guardado en la parte " << j << ": " << part << endl;		
 				descifrado += part[k];
 				j++;
 			}
 			i++;
-			partComparison = part[k];
+			partComparison = part[k]; //guardando en char para poder comparar
 			if (partComparison == 'X') //omitir las X
 				descifrado.erase(descifrado.end()-1); //borra la última que haya metido
 			cout << "descifrado por ahora: " << descifrado << endl;
