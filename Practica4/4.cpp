9bb8a4c07a06788e8798599f86e61f28d683fa9c
Nateri <renato.postigo@ucsp.edu.pe>
2017-05-11 15:24:04 -0500

    Ejercicio 4

--- Practica4/4.cpp
+++ Practica4/4.cpp
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 using namespace std;
 
-long valAbs(long a){
-	if (a < 0)
-		return -a;
-	else return a;
+long valAbs(long a, long b){
+	if ((a-b) < 0)
+		return b-a;
+	else return a-b;
 }
 
 long Algoritmoxx(long a, long b){
@@ -20,9 +20,9 @@ long Algoritmoxx(long a, long b){
 	else if ((a&1) && !(b&1))
 		return 2 * Algoritmoxx(a, b>>1);
 	else{
-		long res = a-b;
-		res>>=2;
-		return Algoritmoxx(abs(res), b);
+		long res = valAbs(a, b);
+		valAbs >>= 1;
+		return Algoritmoxx(res, b);
 	}
 }
 
