b9a70a6be94db25be9202dff2537dbedeb341f9b
Nateri <renato.postigo@ucsp.edu.pe>
2017-05-18 13:45:29 -0500

    cifrado

--- rsaBlocks.cpp
+++ rsaBlocks.cpp
@@ -26,7 +26,7 @@ RSABlocks::RSABlocks(NTL::ZZ a, NTL::ZZ b){
 }
 std::string RSABlocks::cifrar(std::string msj){
 	string leng = zToString(this->N), c, original, tmp;
-	long k = leng.size() - 1;
+	long k = leng.size() - 1; ///k = digitos de N - 1s
 	long i, found;
 	bool euler;
 	NTL::ZZ result, temp, ten;
@@ -36,20 +36,20 @@ std::string RSABlocks::cifrar(std::string msj){
 		euler = true; ///si se cumple, result = a mod n
 	} else ///si no, se realiza la potenciacion modular
 		euler = false;
-	for (i = 0; i < msj.size(); i++){
-		found = this->alfabeto.find(msj[i]); //la letra
-		if (found < 10){
+	for (i = 0; i < msj.size(); i++){ ///llenando los bloques con 2 digitos (tam de la letra mas grande)
+		found = this->alfabeto.find(msj[i]);
+		if (found < 10){ ///si es menor a 10, le metemos un 0 al comienzo
 			original += "0";
 		}
 		ostringstream conv;
 		conv << found;
-		original += conv.str();
+		original += conv.str(); ///guardamos ese numero en un string
 	}
-	ten = potenciacion(NTL::to_ZZ(10), k- NTL::to_ZZ(1)); ///10^(k-1) para saber si result tiene tantos digitos como k
+	ten = potenciacion(NTL::to_ZZ(10), k); ///10^(k) para saber si result tiene tantos digitos como k
 	for(i = 0; i < original.size(); i+=k){
 		tmp.clear();
 		for(short j = 0; j < k; j++){
-			tmp += original[i+j];
+			tmp += original[i+j]; ///saca el bloque de digitos de N - 1
 		}
 		stringstream convi(tmp);
 		convi >> temp; ///string tmp to int (NTL) temp
@@ -57,8 +57,9 @@ std::string RSABlocks::cifrar(std::string msj){
 			result = ntlModulo(temp, this->N); //teorema de euler
 		else
 			result = ntlPotenModular(temp,this->e, this->N); //cifrado en si
-		if (result  < ten){
+		while (result  < ten){ ///si result es menor a 10^k, se le agrega 0 y se divide entre 10 a 10^k (10^{k-1})
 			c += "0";
+			ten /= 10;
 		}
 		ostringstream conv;
 		conv << result;
