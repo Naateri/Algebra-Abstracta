dcd85f62477fa3cd2c5f597f58cb16af9978fd33
Nateri <renato.postigo@ucsp.edu.pe>
2017-06-20 14:21:42 -0500

    Just missing the image

--- protocolo.cpp
+++ protocolo.cpp
@@ -14,6 +14,7 @@ Protocolo::Protocolo(int bits){
 	NTL::ZZ phi;
 	this->myQ = ga(bits, bits>>1, 1, 1);
 	this->myQ = NTL::NextPrime(this->myQ, 1);
+	cout << "Q: " << this->myQ << endl;
 	do{
 		this->myG = ga(bits, bits>>1, 1, 1);
 	}while(this->myG > this->myQ);
@@ -22,14 +23,19 @@ Protocolo::Protocolo(int bits){
 	this->p_2 = ga(bits, bits>>1, 1, 1);
 	this->p_2 = NTL::NextPrime(this->p_2, 1);
 	this->myN = this->p_1 * this->p_2;
+	cout << "N: " << this->myN << endl;
 	phi = (this->p_1 - 1) * (this->p_2 - 1);
 	do{
 		this->myE = ga(bits, bits>>1, 1, 1);
-	}while(mcdNTL(e, phi) != 1); ///e siempre va a ser mayor que 1
-	this->d = inversaNTL(e, phi);
+		cout << "MCD: " << mcdNTL(this->myE, phi) << endl;
+	}while(mcdNTL(this->myE, phi) != 1); ///e siempre va a ser mayor que 1
+	this->d = inversaNTL(this->myE, phi);
+	cout << "d: " << this->d << endl;
 	do{
-		a = ga(bits, bits>>1, 1, 1); ///aleatorio para cifrar
-	}while(a > this->q);
+		this->a = ga(bits, bits>>1, 1, 1); ///aleatorio para cifrar
+		cout << "a temp: " << this->a << endl;
+	}while(this->a > this->myQ);
+	cout << "Claves generadas.\n";
 }
 
 string Protocolo::cifrar(string msj){
@@ -40,6 +46,7 @@ string Protocolo::cifrar(string msj){
 	NTL::ZZ result, temp, ten, ten2, ka, Ca;
 	ka = modExponentiation1(this->g, this->a, this->q);
 	Ca = modExponentiation1(this->a, this->e, this->N);
+	cout << "Ca: " << Ca << endl;
 	ten = potenciacion(NTL::to_ZZ(10), NTL::to_ZZ(lengAlf));
 	for (i = 0; i < msj.size(); i++){ ///llenando los bloques con 2 digitos (tam de la letra mas grande)
 		found = this->alfabeto.find(msj[i]);
@@ -81,8 +88,43 @@ string Protocolo::cifrar(string msj){
 }
 
 string Protocolo::descifra_mensaje(string c){
-	string d;
-	return d;
+	long alfL = this->alfabeto.size();
+	string leng = zToString(this->N), d, ret, tmp, ca, alfLeng = zToString(NTL::to_ZZ(alfL));
+	long k = leng.size(), lengAlf = alfLeng.size(); ///P Digitos
+	long i, found;
+	NTL::ZZ result, temp, ten, ten2, invKM, A, caN, ka;
+	//ca(c.begin() + c.size()-this->N, this->N);
+	ca = c.substr(c.size()-k, k);
+	leng = zToString(this->q);
+	k = leng.size();
+	caN = stringToZZ(ca);
+	cout << "Ca: " << caN << endl;
+	A = modExponentiation1(caN, this->d, this->N);
+	ka = modExponentiation1(this->g, A, this->q);
+	invKM = inversaNTL(ka, this->q);
+	ten = potenciacion(NTL::to_ZZ(10), k-NTL::to_ZZ(2)); ///10^{N-2} para ver si el numero es menor a eso
+	for(i = 0; i < c.size() - k; i+=k){
+		tmp.clear(); ///borrando datos del string
+		ten2 = ten;
+		tmp = c.substr(i, k);
+		temp = stringToZZ(tmp);
+		result = ntlModulo((temp * invKM), this->q);
+		cout << "Result: " << result << endl;
+		ostringstream conv;
+		conv << result;
+		while (result < ten2){ ///guardando en bloques N-1
+			d += "0";
+			ten2 /= 10;
+		}
+		d += conv.str();
+	}
+	for(i = 0; i < d.size(); i+=lengAlf){
+		tmp.clear();
+		tmp = d.substr(i, lengAlf);
+		found = NTL::to_long(stringToZZ(tmp));
+		ret += this->alfabeto[found];
+	}
+	return ret;
 }
 
 NTL::ZZ Protocolo::getE(){return this->e;} ///e del receptor
