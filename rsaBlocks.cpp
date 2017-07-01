bf41d667598a3bdce84b214e7558acfa6258768e
Nateri <renato.postigo@ucsp.edu.pe>
2017-07-01 04:49:19 -0500

    RSA mejoradaso opsaso pls diosito ayudame

--- rsaBlocks.cpp
+++ rsaBlocks.cpp
@@ -10,15 +10,17 @@ using namespace std;
 RSABlocks::RSABlocks(int bits){
 	//testear primalidad de p y q
 	NTL::ZZ pub;
-	this->p = ga(bits, bits/2, 1, 1);
-	this->p = NTL::NextPrime(this->p, 1);
+	/*this->p = ga(bits, bits/2, 1, 1);
+	this->p = NTL::NextPrime(this->p, 1);*/
+	this->p = DES(bits);
 	//this->p = NTL::NextPrime(this->p, 1);
-	this->q = ga(bits, bits/2, 1, 1);
-	this->q = NTL::NextPrime(this->q, 1);
+	/*this->q = ga(bits, bits/2, 1, 1);
+	this->q = NTL::NextPrime(this->q, 1);*/
+	this->q = DES(bits);
 	this->N = p*q;
 	this->phi = (p - 1) * (q - 1);
 	do{
-		pub = ga(bits, bits/2, 1, 1);
+		pub = ga(bits>>1, bits>>3, 1, 1);
 	}while(mcdNTL(pub, this->phi) != 1 || pub >= this->N); //e debe ser menor que N
 	this->e = pub;
 	this->d = inversaNTL(pub, this->phi);
@@ -29,8 +31,9 @@ RSABlocks::RSABlocks(NTL::ZZ ee, NTL::ZZ eN){
 	this->phi = this->e*this->N; ///valor temporal para no tener que modificar cifrado y euler
 }
 std::string RSABlocks::cifrar(std::string msj){
-	string leng = zToString(this->N), c, original, tmp;
-	long k = leng.size() - 1, modd; ///k = digitos de N - 1s
+	long alfL = this->alfabeto.size();
+	string leng = zToString(this->N), c, original, tmp, alfLeng = zToString(NTL::to_ZZ(alfL));
+	long k = leng.size() - 1, modd, lengAlf = alfLeng.size()-1; ///k = digitos de N - 1s
 	long i, found;
 	bool euler;
 	NTL::ZZ result, temp, ten, ten2;
@@ -40,14 +43,15 @@ std::string RSABlocks::cifrar(std::string msj){
 		euler = true; ///si se cumple, result = a mod n
 	} else ///si no, se realiza la potenciacion modular
 		euler = false;
+	ten = potenciacion(NTL::to_ZZ(10), NTL::to_ZZ(lengAlf));
 	for (i = 0; i < msj.size(); i++){ ///llenando los bloques con 2 digitos (tam de la letra mas grande)
 		found = this->alfabeto.find(msj[i]);
-		if (found < 10){ ///si es menor a 10, le metemos un 0 al comienzo
+		ten2 = ten;
+		if (found < ten2){ ///si es menor a 10, le metemos un 0 al comienzo
 			original += "0";
+			ten2 /= 10;
 		}
-		ostringstream conv;
-		conv << found;
-		original += conv.str(); ///guardamos ese numero en un string
+		original += zToString(NTL::to_ZZ(found)); ///guardamos ese numero en un string
 	}
 	modd = modulo(original.size(), k);
 	while(modd != 0){ ///agrega "26" (espacio) mientras la long del texto
@@ -58,11 +62,8 @@ std::string RSABlocks::cifrar(std::string msj){
 	for(i = 0; i < original.size(); i+=k){
 		tmp.clear();
 		ten2 = ten;
-		for(short j = 0; j < k; j++){
-			tmp += original[i+j]; ///saca el bloque de digitos de N - 1
-		}
-		stringstream convi(tmp);
-		convi >> temp; ///string tmp to int (NTL) temp
+		tmp = original.substr(i,k);
+		temp = stringToZZ(tmp); ///string tmp to int (NTL) temp
 		if (euler)
 			result = ntlModulo(temp, this->N); //teorema de euler
 		else
@@ -71,16 +72,15 @@ std::string RSABlocks::cifrar(std::string msj){
 			c += "0";
 			ten2 /= 10;
 		}
-		ostringstream conv;
-		conv << result;
-		c += conv.str(); ///c + "result"
+		c += zToString(result); ///c + "result"
 	}
 	//modd = modulo(c.size() , k+1);
 	return c;
 }
 std::string RSABlocks::descifra_mensaje(std::string c){
-	string leng = zToString(this->N), d, ret, tmp;
-	long k = leng.size(); ///N Digitos
+	long alfL = this->alfabeto.size();
+	string leng = zToString(this->N), d, ret, tmp, alfLeng = zToString(NTL::to_ZZ(alfL));
+	long k = leng.size(), lengAlf = alfLeng.size(); ///N Digitos
 	long i, found;//, hundred = 100, hun;
 	NTL::ZZ result, temp, ten, ten2;
 	ten = potenciacion(NTL::to_ZZ(10), k-NTL::to_ZZ(2)); ///10^{N-2} para ver si el numero es menor a eso
@@ -95,29 +95,21 @@ std::string RSABlocks::descifra_mensaje(std::string c){
 		b.clear(); ///limpiando valores anteriores del vector
 		tmp.clear(); ///borrando datos del string        //ten2 = ten;
 		ten2 = ten;
-		for(short j = 0; j < k; j++){
-			tmp += c[i+j]; ///guardando en tmp de N en N digitos
-		}
-		stringstream convi(tmp);
-		convi >> temp; ///string tmp to int (NTL) temp
+		tmp = c.substr(i,k);
+		temp = stringToZZ(tmp);///string tmp to int (NTL) temp
 		b.push_back(modExponentiation1(temp, dp, p));
 		b.push_back(modExponentiation1(temp, dq, q));
 		result = TCR(a, b, n);
-		ostringstream conv;
-		conv << result;
 		while (result < ten2){ ///guardando en bloques N-1
 			d += "0";
 			ten2 /= 10;
 		}
-		d += conv.str();
+		d += zToString(result);
 	}
-	for(i = 0; i < d.size(); i+=2){
+	for(i = 0; i < d.size(); i+=lengAlf){
 		tmp.clear();
-		for(short j = 0; j < 2; j++){ ///j<2 porque 2 es el tamaño mas grande del alfabeto
-			tmp += d[i+j]; ///guardando en tmp de 2 en 2
-		}
-		stringstream convi(tmp);
-		convi >> found;
+		tmp = d.substr(i, lengAlf);
+		found = NTL::to_int(stringToZZ(tmp));
 		ret += this->alfabeto[found];
 	}
 	return ret;
